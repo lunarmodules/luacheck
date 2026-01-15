724caa491848755ea7a926e31910ad5cdc79ff4b
luau-project <luau.project@gmail.com>
2026-01-15 17:22:01 -0300

    rename `field_name` to `fn` to prevent shadowing error
    * @Aire-One suggestion
    
    Co-authored-by: Aire-One <aireone@aireone.xyz>

--- src/luacheck/standards.lua
+++ src/luacheck/standards.lua
@@ -128,8 +128,8 @@ local function add_fields(def, fields, overwrite, ignore_array_part, default_rea
       return
    end
 
-   for field_name, field_def in pairs(fields) do
-      local field_name = field_name
+   for fn, field_def in pairs(fields) do
+      local field_name = fn
       if type(field_name) == "string" or not ignore_array_part then
          if type(field_name) ~= "string" then
             field_name = field_def
